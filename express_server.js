baa322311619859881b526d03569688312281534
Christopher Rose <48345024+ChristopherRose13@users.noreply.github.com>
2022-01-24 14:41:30 -0800

    added some new paths

--- express_server.js
+++ express_server.js
@@ -11,6 +11,14 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
