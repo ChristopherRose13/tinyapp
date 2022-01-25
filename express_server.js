9ed47fb10403c2f831d60196007ca4f075e5a51a
Christopher Rose <48345024+ChristopherRose13@users.noreply.github.com>
2022-01-24 16:59:30 -0800

    added short URL

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -13,9 +14,16 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls.json", (req, res) =>  {
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
 });
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL:"/urls/:shortURL" };
+  res.render("urls_show", templateVars);
+});
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
